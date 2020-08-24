24a4ab9656b9d131f910a1b63b6deff97b81259e
Renan Miranda <renanlmiranda@gmail.com>
2020-08-24 02:23:45 -0300

    finish

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 import { FiPlusSquare } from 'react-icons/fi';
@@ -15,12 +16,7 @@ const Header: React.FC<IHeaderProps> = ({ openModal }) => (
       <img src={Logo} alt="GoRestaurant" />
       <nav>
         <div>
-          <button
-            type="button"
-            onClick={() => {
-              /* TODO OPEN MODAL */
-            }}
-          >
+          <button type="button" onClick={() => openModal()}>
             <div className="text">Novo Prato</div>
             <div className="icon">
               <FiPlusSquare size={24} />
